a17e876962077aed9f0c37f6f3009c0466d92652
gaabs <gaabs@cin.ufpe.br>
2016-01-06 19:32:36 -0300

    Spring Security plugin

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -115,3 +115,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'teste.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'teste.UserRole'
+grails.plugin.springsecurity.authority.className = 'teste.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll']
+]
+
